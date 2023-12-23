9abca0ef6ab5d9bbca11d34845ed56ca937710b8
jim1286 <jimin1286@gmail.com>
2023-12-23 14:26:27 +0900

    [Fix] fix ci cd

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -3,15 +3,13 @@ import * as config from 'config';
 
 const dbConfig = config.get('db');
 
-console.log(dbConfig);
-
 export const typeORMConfig: TypeOrmModuleOptions = {
   type: dbConfig.type,
-  host: dbConfig.host,
-  port: dbConfig.port,
-  username: dbConfig.username,
-  password: dbConfig.password,
-  database: dbConfig.database,
+  host: process.env.RDS_HOSTNAME || dbConfig.host,
+  port: process.env.RDS_PORT || dbConfig.port,
+  username: process.env.RDS_USERNAME || dbConfig.username,
+  password: process.env.RDS_PASSWORD || dbConfig.password,
+  database: process.env.RDS_DB_NAME || dbConfig.database,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: dbConfig.synchronize,
 };
